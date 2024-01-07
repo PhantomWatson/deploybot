f9ee0106ea3b22cef902fadf4013bf48d0465762
Graham Watson <graham@phantomwatson.com>
2024-01-06 22:16:40 -0500

    Set COMPOSER_HOME
    to fix "The HOME or COMPOSER_HOME environment variable must be set for composer to run correctly" alert

--- config/commands.php
+++ config/commands.php
@@ -9,6 +9,7 @@ $composer = "$php /home/phanto41/public_html/deploybot/composer.phar";
 return [
     'git pull',
     'git status',
+    "export COMPOSER_HOME=\"~/.config/composer/\"",
     "$composer self-update",
     "$composer install --no-dev",
 ];
